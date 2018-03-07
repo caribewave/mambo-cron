3eff5eeb7364158b7fe9e8ab4e9168a65e61c46d
nbelleme <nbelleme@e-biz.fr>
2018-03-07 15:41:10 +0100

    dumb api

--- src/PlaneController.js
+++ src/PlaneController.js
@@ -26,6 +26,9 @@ async function onGetLoc(req, res) {
   let lat = req.query.lat;
   let lng = req.query.lng;
 
+  lat = 49.602247;
+  lng = 1.339664;
+
   let min = req.query.min || 0;
   let max = req.query.max || 10000;
 
@@ -43,7 +46,18 @@ async function onGetLoc(req, res) {
   };
 
 
-  let result = await Plane.find(query);
+  // let result = await Plane.find(query);
+
+  result = [{
+    "location": {"type": "Point", "coordinates": [[49.602247, 1.339664], [49.602247, 1.339666g]]},
+    "_id": "5a9ff37e3219088decdfd69a",
+    "hex": "400685",
+    "seen_pos": 5.1,
+    "altitude": 36700,
+    "messages": 10,
+    "seen": 4.3,
+    "__v": 0
+  }];
   res.end(JSON.stringify(result));
 }
 
